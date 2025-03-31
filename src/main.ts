a2618bc07aa6313c1a6002ed5531d8be46519bbe
Shakim Harris <amenraprod@gmail.com>
2025-03-31 04:10:15 -0400

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+    //All headers that client is allowed to use
+    allowedHeaders: [
+      'Accept',
+      'Authorization',
+      'Content-Type',
+      'X-Requested-Width',
+      'apollo-require-preflight'
+
+    ], 
+    methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS']
+  })
   await app.listen(3000);
 }
 bootstrap();
