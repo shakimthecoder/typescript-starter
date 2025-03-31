c5b71812c44372b82f4fc6960dafb96f1c7ddb48
Shakim Harris <amenraprod@gmail.com>
2025-03-31 04:34:56 -0400

    fix import

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthResolver } from './auth.resolver';
 import { AuthService } from './auth.service';
 
+
 @Module({
   providers: [AuthResolver, AuthService]
 })
