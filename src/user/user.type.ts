16a3d42d33dd6a3d23f6edf93820840a63e3cc86
Shakim Harris <amenraprod@gmail.com>
2025-03-31 04:04:04 -0400

    create user type

---
+++ src/user/user.type.ts
@@ -0,0 +1,23 @@
+import { Field, ObjectType } from "@nestjs/graphql"
+
+@ObjectType()
+export class User {
+    @Field({ nullable: true})
+    id ?: number;
+
+    @Field()
+    fullname: string;
+
+    @Field()
+    email ?: string;
+
+    @Field({ nullable: true })
+    avatarUrl ?: string;
+
+    @Field({ nullable: true })
+    createdAt ?: Date;
+
+    @Field({ nullable: true })
+    updatedAt ?: Date;
+
+}
