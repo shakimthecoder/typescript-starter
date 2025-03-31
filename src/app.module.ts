39e26c942ca5b965ed539dafc4a916046ec5b3cf
Shakim Harris <amenraprod@gmail.com>
2025-03-31 04:48:50 -0400

    update module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,29 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { ApolloDriver } from '@nestjs/apollo';
+import { ConfigModule, ConfigService } from '@nestjs/config'
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, UserModule,
+     GraphQLModule.forRootAsync({
+      imports: [ConfigModule, AppModule],
+      inject: [ConfigService],
+      driver: ApolloDriver,
+      useFactory: async (
+        configService: ConfigService,
+      ) => {
+        return {
+          playground: true,
+          autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+          sortSchema: true,
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
