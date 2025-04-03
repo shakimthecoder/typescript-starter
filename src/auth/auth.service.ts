99a91d163b0479542ef855efe205e1d30853c86b
Shakim Harris <amenraprod@gmail.com>
2025-04-03 17:56:03 -0400

    payload check

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,20 +2,27 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt'
 import { PrismaService } from 'src/prisma.service';
-import { Request } from 'express'''
+import { Request } from 'express';
 @Injectable()
 export class AuthService {
     constructor(
         private readonly jwtService: JwtService,
         private readonly prisma: PrismaService,
         private readonly configService: ConfigService
-    ){
+    ){}
         async refreshToken(req: Request, res: Response){
            const refreshToken = req.cookies['refresh_token'];
 
            if(!refreshToken){
             throw new UnauthorizedException('Token not found');
+            }
+           let payload;
+           try {
+               payload = this.jwtService.verify(refreshToken, {
+                secret: this.configService.get<string>('REFRESH_TOKEN_SECRET');
+               })
+           } catch {
+
            }
         }
     }
-}
