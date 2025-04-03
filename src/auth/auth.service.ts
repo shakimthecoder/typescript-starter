ff2043cc40f601e2bf3dcf4b011f7cf37482e2da
Shakim Harris <amenraprod@gmail.com>
2025-04-03 17:49:44 -0400

    inject services

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
-
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt'
+import { PrismaService } from 'src/prisma.service';
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private readonly jwtService: JwtService,
+        private readonly prisma: PrismaService,
+        private readonly configService: ConfigService
+    ){
+        
+    }
+}
