df527d2665e35f085a24e0611bb14d5963ecd0d6
Shakim Harris <amenraprod@gmail.com>
2025-04-04 02:11:56 -0400

    add mutation for registration

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,7 +1,31 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, Context } from '@nestjs/graphql';
+import { AuthService } from './auth.service';
+import { RegisterResponse } from './types';
+import { RegisterDto } from './dto';
+import { Response } from 'express';
+import { BadRequestException } from '@nestjs/common';
+
+
 
 @Resolver()
 export class AuthResolver {
+    constructor(
+        private readonly authService: AuthService
+    ){}
+   @Mutation(() => RegisterResponse)
+    async register(
+        @Args('registerInput') registerDTo: RegisterDto,
+        @Context() context: { res: Response },
+         ) {
+            if(registerDTo.password !== registerDTo.confirmPassword){
+                throw new BadRequestException({
+                    confirmPassword: 'Password do not match'
+                });
+            }
+            const { user } = await this.authService.registerUser(registerDTo, context.res);
+            return { user };
+         }
+
     @Query(() => String)
     async hello(){
         return 'hello';
