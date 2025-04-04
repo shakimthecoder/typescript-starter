f7e1c1cf38089e990cacecccfe72cf1c35d7b6d4
Shakim Harris <amenraprod@gmail.com>
2025-04-04 02:43:37 -0400

    setting up mutation

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,4 +1,27 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Context, Mutation, Args, Query } from '@nestjs/graphql';
+import { UserService } from './user.service';
+import { User } from './user.type';
+import { UseGuards } from '@nestjs/common';
+import { GraphQLAuthGuard } from 'src/auth/graphlql-auth-guard';
+import { createWriteStream } from 'fs';
+import { join } from 'path';
+import { v4 as uuidv4 } from 'uuid';
+import * as GraphQlUploadExpress from 'graphql-upload/graphqlUploadExpress.mjs';
 
 @Resolver()
-export class UserResolver {}
+export class UserResolver {
+    constructor(
+     private readonly userService: UserService,
+    ){}
+
+    @UseGuards(GraphQLAuthGuard)
+    @Mutation(() => User)
+    async updateProfile(
+        @Args('fullname') fullname: string,
+        @Args('file', { type: () => GraphQlUploadExpress.GraphQLUpload, nullable: true})
+        file: GraphQlUploadExpress.file,
+        @Context() context: { req: Request } , ) {
+            const imageUrl = file ? 
+        }
+    )
+}
