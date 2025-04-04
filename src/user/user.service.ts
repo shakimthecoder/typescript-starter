20bc356d95b8e09ea98436e699f49019388e1b49
Shakim Harris <amenraprod@gmail.com>
2025-04-04 02:25:28 -0400

    add method to update profile

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(
+        private readonly prisma: PrismaService,
+    ){ }
+    async updateProfile(userId: number, fullname: string, avatarUrl: string){
+        return await this.prisma.user.update({
+            where: { id: userId },
+            data: {
+                fullname, 
+                avatarUrl
+            }
+        })
+    }
+}
