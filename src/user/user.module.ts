f31d1401048007c016e78349d14ee821f8c312b5
Shakim Harris <amenraprod@gmail.com>
2025-04-04 03:01:38 -0400

    add service providers

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserResolver } from './user.resolver';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from '../prisma.service'; // Ensure the path is correct
 
 @Module({
-  providers: [UserService, UserResolver]
+  providers: [UserService, UserResolver, PrismaService, JwtService]
 })
 export class UserModule {}
