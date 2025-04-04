d29f1a352e24477b61fe519a93c2d8a1f6dfd0a8
Shakim Harris <amenraprod@gmail.com>
2025-04-04 03:01:43 -0400

    add service provider

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthResolver } from './auth.resolver';
 import { AuthService } from './auth.service';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from '../prisma.service'; // Ensure the path is correct
 
 
 @Module({
-  providers: [AuthResolver, AuthService]
+  providers: [AuthResolver, AuthService, JwtService, PrismaService]
 })
 export class AuthModule {}
